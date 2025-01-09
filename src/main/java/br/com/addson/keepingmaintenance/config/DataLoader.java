256790a4a01bb640f5e2dd6f69ce677925d96c64
Weslley Addson <wesquel@hotmail.com>
2025-01-08 22:21:36 -0300

    feat: Update DataLoader to insert DeviceTypes and Components along with Statuses

--- src/main/java/br/com/addson/keepingmaintenance/config/DataLoader.java
+++ src/main/java/br/com/addson/keepingmaintenance/config/DataLoader.java
@@ -1,19 +1,27 @@
 package br.com.addson.keepingmaintenance.config;
 
+import br.com.addson.keepingmaintenance.model.Component;
+import br.com.addson.keepingmaintenance.model.DeviceType;
 import br.com.addson.keepingmaintenance.model.Status;
+import br.com.addson.keepingmaintenance.repository.ComponentRepository;
+import br.com.addson.keepingmaintenance.repository.DeviceTypeRepository;
 import br.com.addson.keepingmaintenance.repository.StatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
-@Component
+@org.springframework.stereotype.Component
 public class DataLoader implements CommandLineRunner {
 
+    private final DeviceTypeRepository deviceTypeRepository;
+    private final ComponentRepository componentRepository;
     private final StatusRepository statusRepository;
 
-    public DataLoader(StatusRepository statusRepository) {
+    public DataLoader(DeviceTypeRepository deviceTypeRepository, ComponentRepository componentRepository, StatusRepository statusRepository) {
+        this.deviceTypeRepository = deviceTypeRepository;
+        this.componentRepository = componentRepository;
         this.statusRepository = statusRepository;
     }
 
@@ -27,6 +35,23 @@ public class DataLoader implements CommandLineRunner {
                 new Status(null, "Parts Required", "The device is operational but requires specific parts for optimal performance.")
             );
             statusRepository.saveAll(status);
+        }
+
+        // Inserir Tipos de Dispositivos
+        if (deviceTypeRepository.count() == 0) {
+            DeviceType computerType = new DeviceType("Computer");
+            deviceTypeRepository.saveAll(List.of(computerType));
+
+            Status status = statusRepository.findById(1L).get();
+
+            // Inserir Componentes para cada Tipo de Dispositivo
+            List<Component> computerComponents = List.of(
+                    new Component("HD", "Hard Drive", status, computerType),
+                    new Component("RAM", "Memory", status, computerType),
+                    new Component("CPU", "Processor", status, computerType)
+            );
+
+            componentRepository.saveAll(computerComponents);
         }
     }
 }
