7dc97ef82b87b6675494be958b3ceddc99c99f03
Weslley Addson <wesquel@hotmail.com>
2025-01-11 12:12:31 -0300

    chore: refactor imports

--- src/main/java/br/com/addson/keepingmaintenance/dto/component/ComponentRequest.java
+++ src/main/java/br/com/addson/keepingmaintenance/dto/component/ComponentRequest.java
@@ -1,7 +1,4 @@
 package br.com.addson.keepingmaintenance.dto.component;
 
-import br.com.addson.keepingmaintenance.dto.status.StatusRequest;
-import br.com.addson.keepingmaintenance.model.Component;
-
 public record ComponentRequest(Long id) {
 }
--- src/main/java/br/com/addson/keepingmaintenance/repository/StatusRepository.java
+++ src/main/java/br/com/addson/keepingmaintenance/repository/StatusRepository.java
@@ -1,9 +1,7 @@
 package br.com.addson.keepingmaintenance.repository;
 
 import br.com.addson.keepingmaintenance.model.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;;
 
-@Repository
 public interface StatusRepository extends JpaRepository<Status, Long> {
 }
