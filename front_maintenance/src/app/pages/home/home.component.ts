649f5bd41012f7da2ce93644c1efc669279b8d9f
Weslley Addson <wesquel@hotmail.com>
2025-01-12 13:56:24 -0300

    feat: create home page.

--- front_maintenance/src/app/pages/home/home.component.ts
+++ front_maintenance/src/app/pages/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { TableMaintenanceComponent } from "../../components/table-maintenance/table-maintenance.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatFormFieldModule, MatIconModule, MatInputModule, TableMaintenanceComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
 
