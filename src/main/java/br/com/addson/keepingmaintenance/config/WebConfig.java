7ddaae770822fba05c95ac93ca1b9df76a37fa3d
Weslley Addson <wesquel@hotmail.com>
2025-01-19 12:34:28 -0300

    feat: add WebConfig

---
+++ src/main/java/br/com/addson/keepingmaintenance/config/WebConfig.java
@@ -0,0 +1,25 @@
+package br.com.addson.keepingmaintenance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas as rotas
+                        .allowedOrigins("http://localhost:4200") // Permitir o front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Cabeçalhos permitidos
+                        .allowCredentials(true); // Permitir credenciais (opcional)
+            }
+        };
+    }
+}
+
