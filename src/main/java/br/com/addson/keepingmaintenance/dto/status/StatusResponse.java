9b896ec1d361b8f81dfceb835b206f7563c9478d
Weslley Addson <wesquel@hotmail.com>
2025-01-19 23:17:48 -0300

    chore: update parameter nome to name.

--- src/main/java/br/com/addson/keepingmaintenance/dto/status/StatusResponse.java
+++ src/main/java/br/com/addson/keepingmaintenance/dto/status/StatusResponse.java
@@ -2,7 +2,7 @@ package br.com.addson.keepingmaintenance.dto.status;
 
 import br.com.addson.keepingmaintenance.model.Status;
 
-public record StatusResponse(Long id, String nome, String description) {
+public record StatusResponse(Long id, String name, String description) {
     public static StatusResponse fromEntity(Status status) {
         return new StatusResponse(status.getId(), status.getName(), status.getDescription());
     }
